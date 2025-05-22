60587d3e350d4688c43e0de9582b4ede6359ebf5
Mikhail Ratcliff <1p3til@gmail.com>
2025-05-22 13:28:19 -0500

    Added a sign up/in

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ const app = express();
 app.get('/favicon.ico', (req, res) => {res.sendFile(path.join(__dirname, '/favicon.ico'))})
 
 app.get(['/*p', '/'], (req, res) => {
-    // Redirect to basic page without .html in req.path
-    if (req.path.endsWith('.html')) {
-        return  res.redirect(301, req.path.replace('.html', ''));
-    }
-
-    // If the file requested doesn't exist then.. 404!!
-    if (!(fs.existsSync(path.join(__dirname, req.path)) 
-        || fs.existsSync(path.join(__dirname, req.path, 'index.html')))) {
-            return res.status(404).sendFile(path.join(__dirname, 'error/404.html'));
-        }
-    
-    // Determines if it is a index.html or other file
-    if (req.path.split('/')[req.path.split('/').length - 1].includes('.')) {
-        return res.sendFile(path.join(__dirname, req.path));
-    }
-
-
     // $ File Work
     function $(filec, step=1) {
         if (step == 10) {return;}
@@ -44,8 +27,25 @@ app.get(['/*p', '/'], (req, res) => {
         return filew;
     }
 
-    // If non other reply with index for that page
-    return res.send($(fs.readFileSync(path.join(__dirname, req.path, 'src/index.html'), 'utf8')));
+    // Redirect to basic page without .html in req.path
+    if (req.path.endsWith('.html')) {
+        return res.redirect(301, req.path.replace('.html', ''));
+    }
+
+    // If the file requested doesn't exist then.. 404!!
+    if (!(fs.existsSync(path.join(__dirname, 'src', req.path)) 
+        || fs.existsSync(path.join(__dirname, 'src', req.path, 'index.html')))) {
+            return res.status(404).send($(fs.readFileSync(path.join(__dirname, 'error/404.html'), 'utf-8')));
+        }
+    
+    // Determines if it is a index.html or other file
+    if (req.path.split('/')[req.path.split('/').length - 1].includes('.')) {
+        return res.send($(fs.readFileSync(path.join(__dirname, req.path), 'utf-8')));
+    }
+
+    if (!(req.path.split('/')[req.path.split('/').length - 1].includes('.'))) {
+        return res.send($(fs.readFileSync(path.join(__dirname, 'src', req.path, 'index.html'), 'utf8')));
+    }
 });
 
 app.listen(80);
